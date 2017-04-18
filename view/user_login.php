744ebf95a8e07a01da9f54a294f1dc73912991ae
larissariesen <larissa.riesen@hotmail.com>
2017-04-18 13:24:16 +0200

    Project Import

---
+++ view/user_login.php
@@ -0,0 +1,22 @@
+<form class="form-horizontal" action="/user/doLogin" method="post">
+    <div class="component" data-html="true">
+        <div class="form-group">
+            <label class="col-md-2 control-label" for="username">Username</label>
+            <div class="col-md-4">
+                <input id="username" name="username" type="text" placeholder="E-mail" class="form-control input-md">
+            </div>
+        </div>
+        <div class="form-group">
+            <label class="col-md-2 control-label" for="password">Password</label>
+            <div class="col-md-4">
+                <input id="password" name="password" type="password" placeholder="Password" class="form-control input-md">
+            </div>
+        </div>
+        <div class="form-group">
+            <label class="col-md-2 control-label" for="login">&nbsp;</label>
+            <div class="col-md-4">
+                <input id="login" name="login" type="submit" class="btn btn-primary">
+            </div>
+        </div>
+    </div>
+</form>
