efd963d9f682894957277a6c0fc0dbfa92c58097
Max <mebinmebin@hotmail.com>
2017-04-19 16:29:24 +0200

    not important

--- view/footer.php
+++ view/footer.php
@@ -1,6 +1,6 @@
         <hr>
         <footer>
-            <p>&copy; Blog von Max und Larissa. </p>
+            <p>&copy; Blog von Max und Larissa</p>
         </footer>
     </div>
         <script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
