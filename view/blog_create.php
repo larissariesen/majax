982c754e59efb5bf31fb50802462e67d2adf07a6
larissariesen <larissa.riesen@hotmail.com>
2017-04-20 23:12:55 +0200

    Comments

--- view/blog_create.php
+++ view/blog_create.php
@@ -1,27 +1,27 @@
 <form class="form-horizontal" action="/blog/doCreate" method="post" enctype="multipart/form-data">
     <div class="component" data-html="true">
+        <!-- Title -->
         <div class="form-group">
             <label class="col-md-2 control-label" for="title">Blog Title</label>
             <div class="col-md-4">
                 <input id="title" name="title" type="text" placeholder="Title" class="form-control input-md" required>
             </div>
         </div>
+        <!-- Content -->
         <div class="form-group">
             <label class="col-md-2 control-label" for="content" >Content</label>
             <div class="col-md-4">
                 <textarea id="content" name="content" placeholder="Content" class="form-control input-md"></textarea>
             </div>
         </div>
-
-
+        <!-- Image-->
         <div class="form-group">
             <label class="col-md-2 control-label" for="image_path">Image</label>
             <div class="col-md-4">
                 <input id="image_path" name="image_path" type="file" class="form-control input-md">
             </div>
         </div>
-
-
+        <!-- SubmitButton-->
         <div class="form-group">
             <label class="col-md-2 control-label" for="send">&nbsp;</label>
             <div class="col-md-4">
--- view/blog_edit.php
+++ view/blog_edit.php
@@ -1,11 +1,14 @@
+<!-- get blog_id for edit the right blog -->
 <form class="form-horizontal" action="/blog/doEdit?id=<?= $blog->id ?>" method="post" enctype="multipart/form-data">
     <div class="component" data-html="true">
+        <!-- Title -->
         <div class="form-group">
             <label class="col-md-2 control-label" for="title">Blog Title</label>
             <div class="col-md-4">
                 <input id="title" name="title" type="text" placeholder="Title" class="form-control input-md" required value="<?= $blog->title ?>">
             </div>
         </div>
+        <!-- Content-->
         <div class="form-group">
             <label class="col-md-2 control-label" for="content" >Content</label>
             <div class="col-md-4">
